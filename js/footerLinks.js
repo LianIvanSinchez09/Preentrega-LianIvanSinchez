f5d183008ac234814b89778319e827465ebb94ba
lianivansinchez <maquiavelic1030@gmail.com>
2023-08-24 14:14:52 -0300

    fix al footer

--- js/footerLinks.js
+++ js/footerLinks.js
@@ -1,14 +1,27 @@
-const footerLinks = [
-    {text: "Productos", url: "../pages/productos.html"},
-    {text: "Empleados", url: "../pages/empleados.html"},
-    {text: "Contacto", url: "../pages/contacto.html"},
-    {text: "Newsletter", url: "../pages/newsletter.html"}
-]
 
+// algoritmo que pone links en el footer
+
+// instancia de links
+const footerLinks = [
+    {text: "Productos", url: "/pages/productos.html"},
+    {text: "Empleados", url: "/pages/empleados.html"},
+    {text: "Contacto", url: "/pages/contacto.html"},
+    {text: "Newsletter", url: "/pages/newsletter.html"}
+]
+// const footerLinksIndex = [
+//     {text: "Productos", url: "/pages/productos.html"},
+//     {text: "Empleados", url: "../pages/empleados.html"},
+//     {text: "Contacto", url: "../pages/contacto.html"},
+//     {text: "Newsletter", url: "../pages/newsletter.html"}
+// ]
+
+//seleccionamos el elemento con la clase .list unstyled
 const listaFooter = document.querySelector(".list-unstyled");
 
+// les asignamos un nodo + estilos con la funcion map
 const footerLinksHTML = footerLinks.map(link => `
     <li><a class="nav-item link-nav" href="${link.url}">${link.text}</a></li>
     `);
 
+// se convierte el array en un string con la funcion .join()
 listaFooter.innerHTML = footerLinksHTML.join('');
