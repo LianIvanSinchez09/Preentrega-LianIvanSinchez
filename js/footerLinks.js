2d235e185b3f2533d7678fe910f476c158916e93
lianivansinchez <maquiavelic1030@gmail.com>
2023-08-24 14:18:18 -0300

    fix al footer

--- js/footerLinks.js
+++ js/footerLinks.js
@@ -3,17 +3,17 @@
 
 // instancia de links
 const footerLinks = [
+    {text: "Productos", url: "../pages/productos.html"},
+    {text: "Empleados", url: "../pages/empleados.html"},
+    {text: "Contacto", url: "../pages/contacto.html"},
+    {text: "Newsletter", url: "../pages/newsletter.html"}
+]
+const footerLinksIndex = [
     {text: "Productos", url: "/pages/productos.html"},
     {text: "Empleados", url: "/pages/empleados.html"},
     {text: "Contacto", url: "/pages/contacto.html"},
     {text: "Newsletter", url: "/pages/newsletter.html"}
 ]
-// const footerLinksIndex = [
-//     {text: "Productos", url: "/pages/productos.html"},
-//     {text: "Empleados", url: "../pages/empleados.html"},
-//     {text: "Contacto", url: "../pages/contacto.html"},
-//     {text: "Newsletter", url: "../pages/newsletter.html"}
-// ]
 
 //seleccionamos el elemento con la clase .list unstyled
 const listaFooter = document.querySelector(".list-unstyled");
@@ -25,3 +25,16 @@ const footerLinksHTML = footerLinks.map(link => `
 
 // se convierte el array en un string con la funcion .join()
 listaFooter.innerHTML = footerLinksHTML.join('');
+
+/*                                      */
+
+//seleccionamos el elemento con la clase .list unstyled
+const listaFooterIndex = document.querySelector(".list-unstyled");
+
+// les asignamos un nodo + estilos con la funcion map
+const footerLinksIndexHTML = footerLinksIndex.map(linkIndex => `
+    <li><a class="nav-item link-nav" href="${linkIndex.url}">${linkIndex.text}</a></li>
+    `);
+
+// se convierte el array en un string con la funcion .join()
+listaFooterIndex.innerHTML = footerLinksIndexHTML.join('');
